7c2c9454bdb74fc4c2d4e362a7fcd44504d8f22d
josh <jlcastro@ucsd.edu>
2020-03-10 22:35:21 -0700

    got to bobsleigh

--- DSC100_Project/format_athlete_events.sql
+++ DSC100_Project/format_athlete_events.sql
@@ -10,5 +10,57 @@ WHERE lower(sport) like 'alpine skiing%';
 -- ROLLBACK TO SAVEPOINT pre_update;
 COMMIT;
 
+-- BASQUE PELOTA
+-- begin transaction block
+BEGIN;
+-- add savepoint if needed
+SAVEPOINT pre_update_bp;
+-- change
+UPDATE athlete_events
+SET event = E'Cesta Punta'
+WHERE lower(sport) like 'basque%';
+-- ROLLBACK TO SAVEPOINT pre_update_bp;
+COMMIT;
+
+-- BEACH VOLLEYBALL
+--- IN ATHLETE_EVENTS
+-- begin transaction block
+BEGIN;
+-- add savepoint if needed
+SAVEPOINT pre_update;
+-- change
+UPDATE athlete_events
+SET sport = 'Volleyball'
+WHERE sport ilike 'beach volleyball%';
+
+UPDATE summer
+SET discipline = 'Beach Volleyball'
+where sport ilike 'volley%' and discipline ilike 'beach%';
+-- ROLLBACK TO SAVEPOINT pre_update;
+COMMIT;
+
+-- BIATHLON
+-- begin transaction block
+BEGIN;
+-- add savepoint if needed
+SAVEPOINT pre_update;
+-- change
+UPDATE athlete_events
+SET event = regexp_replace(event, E'( kilometres)+', 'KM', 'g')
+WHERE sport ilike '%biathlon%';
+-- ROLLBACK TO SAVEPOINT pre_update;
+COMMIT;
+
+--BOBSLEIGH
+-- begin transaction block
+BEGIN;
+-- add savepoint if needed
+SAVEPOINT pre_update;
+-- change
+UPDATE winter
+SET event = regexp_replace(event, E'\-[A-Za-z]{3,5}', '')
+WHERE lower(sport) like '%bobsleigh%';
+-- ROLLBACK TO SAVEPOINT pre_update;
+COMMIT;
 
 --
